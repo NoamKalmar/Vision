4c99777b5138068566f465a4aaf81b31620773bf
NoamKalmar <noamkalmar@gmail.com>
2026-04-21 20:20:42 +0300

    fixed the video capture api to be correct on different platforms

--- camera.py
+++ camera.py
@@ -1,11 +1,15 @@
 import time
+import platform
+
 import cv2
+
+CAP_API = cv2.CAP_DSHOW if platform.system() == "Windows" else cv2.CAP_V4L2
 
 class Camera:
     def __init__(self, cap_index: int, num_scan_frames: int) -> None:
         self.cap_index = cap_index
         self.num_scan_frames = num_scan_frames
-        self.cap = cv2.VideoCapture(cap_index, cv2.CAP_V4L2)
+        self.cap = cv2.VideoCapture(cap_index, CAP_API)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
         self.on: bool = True
